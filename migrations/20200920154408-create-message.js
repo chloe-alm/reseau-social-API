3c259bc8839f4983dd452bde14da8c8e2053fe67
MacBook-Pro-de-Chloe <cdargelez@gmail.com>
2020-09-20 21:28:09 +0200

    migration message

---
+++ migrations/20200920154408-create-message.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Messages', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      content: {
+        type: Sequelize.STRING
+      },
+      attachement: {
+        type: Sequelize.STRING
+      },
+      like: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Messages');
+  }
+};
---
+++ models/message.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Message extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Message.init({
+    title: DataTypes.STRING,
+    content: DataTypes.STRING,
+    attachement: DataTypes.STRING,
+    like: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Message',
+  });
+  return Message;
+};
